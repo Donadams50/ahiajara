104f5fe85a101d72824eca69312b6393c6e0e0ba
donadams50 <sumbomatic@gmail.com>
2020-09-02 09:11:57 +0100

    hh

--- app/products/products.routes.js
+++ app/products/products.routes.js
@@ -5,7 +5,7 @@ module.exports = app => {
     require('../Cloudinary/cloudinary.js')
     const upload = require('../Cloudinary/multer.js');
  
-        //console.log("routes")
+        
  app.post("/product",  verifyToken, isAdmin, upload.single("files"), product.create)
  app.get("/products",  verifyToken,  product.findAllProducts)
 }
