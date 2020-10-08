cf7d31a4ea8c7595d63b89673bb58c53ffe188b3
donadams50 <sumbomatic@gmail.com>
2020-10-08 10:16:52 +0100

    update

--- app/skinIssues/skinissues.controller.js
+++ app/skinIssues/skinissues.controller.js
@@ -56,7 +56,7 @@ exports.update = async(req, res) => {
                 _id : req.params.id,
                 category: req.body.category,
                 imgUrl: req.body.files,
-                symptom: req.body.symptom,
+                symptom: JSON.parse(req.body.symptom),
                  name: req.body.name,
                 description: req.body.description     
               });
